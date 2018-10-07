99c67f54aed7ebaec4a9c2aacc3d23e8b4e7356f
ming@poxiaobbs.com <ming@poxiaobbs.com>
2018-10-07 15:57:37 +0800

    瞎玩

---
+++ Test/keywork.py
@@ -0,0 +1,12 @@
+import urllib, urllib2, sys
+
+import ssl
+
+# client_id 为官网获取的AK， client_secret 为官网获取的SK
+host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=277fb7d164f048&client_secret=03c5ab6'
+request = urllib2.Request(host)
+request.add_header('Content-Type', 'application/json; charset=UTF-8')
+response = urllib2.urlopen(request)
+content = response.read()
+if (content):
+    print(content)
